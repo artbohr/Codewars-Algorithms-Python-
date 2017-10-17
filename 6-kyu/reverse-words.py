b290f2002c90f41f104b9270d0ac0dddc0836996
Artur Bordyuzha <26352035+artbohr@users.noreply.github.com>
2017-10-17 18:39:48 +0200

    Update description

--- 6-kyu/reverse-words.py
+++ 6-kyu/reverse-words.py
@@ -1,5 +1,6 @@
 '''
-Write a reverseWords function that accepts a string a parameter, and reverses each word in the string. Every space should stay, so you cannot use words from Prelude.
+Write a reverseWords function that accepts a string a parameter,
+and reverses each word in the string. Every space should stay, so you cannot use words from Prelude.
 
 Example:
 
