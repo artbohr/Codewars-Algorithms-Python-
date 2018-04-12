3bbf537dcec8c54ed617691794afdeefe1ef3816
artbohr <bohrcs@gmail.com>
2018-04-12 09:35:27 +0200

    Create functional-addition.py

---
+++ 7-kyu/functional-addition.py
@@ -0,0 +1,12 @@
+def add(n):
+    return lambda x: x + n
+    
+'''
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+add_one = add(1)
+add_one(3)  # 4
+
+add_three = add(3)
+add_three(3) # 6
+'''
