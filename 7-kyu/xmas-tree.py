ae38050e2931e2400fd424ddf5a9069e16f05458
artbohr <bohrcs@gmail.com>
2018-05-31 17:08:31 +0200

    Create xmas-tree.py

---
+++ 7-kyu/xmas-tree.py
@@ -0,0 +1,33 @@
+def xMasTree(n):
+    res = [(n-i) * '_' + ('#' * (i*2-1)) + ((n-i) * '_') for i in range(1,n)]
+    return res + [(n*2-1) * '#', res[0], res[0]] if n>1 else ['#','#','#']
+
+'''
+Create a function xMasTree(height) that returns a christmas tree of the correct
+height. The height is passed through to the function and the function should
+return a list containing each line of the tree.
+
+xMasTree(5) should return : ['____#____', '___###___', '__#####__', '_#######_',
+'#########', '____#____', '____#____']
+xMasTree(3) should return : ['__#__', '_###_', '#####', '__#__', '__#__']
+
+The final idea is for the tree to look like this if you decide to print each
+element of the list:
+
+xMasTree(5) will result in:
+____#____              1
+___###___              2
+__#####__              3
+_#######_              4
+#########       -----> 5 - Height of Tree
+____#____        1
+____#____        2 - Trunk/Stem of Tree
+
+xMasTree(3) will result in:
+__#__                  1
+_###_                  2
+#####          ----->  3 - Height of Tree
+__#__           1
+__#__           2 - Trunk/Stem of Tree
+
+'''
