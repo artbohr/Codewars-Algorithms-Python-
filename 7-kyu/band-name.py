af9b01c585a30a55c3720a7193907618992a037a
artbohr <bohrcs@gmail.com>
2018-06-28 02:40:34 +0200

    Create band-name.py

---
+++ 7-kyu/band-name.py
@@ -0,0 +1,18 @@
+def band_name_generator(n):
+    return n.capitalize() + n[1:] if n[0]==n[-1] else 'The ' + n.capitalize()
+
+'''
+My friend wants a new band name for her band. She like bands that use the formula:
+"The" + a noun with the first letter capitalized, for example:
+
+"dolphin" -> "The Dolphin"
+
+However, when a noun STARTS and ENDS with the same letter, she likes to repeat the
+noun twice and connect them together with the first and last letter,
+combined into one word (WITHOUT "The" in front), like this:
+
+"alaska" -> "Alaskalaska"
+
+Complete the function that takes a noun as a string,
+and returns her preferred band name written as a string.
+'''
