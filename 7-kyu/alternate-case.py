eebd25c95d605fede13b9367d475429a0028e6d4
artbohr <bohrcs@gmail.com>
2018-07-14 04:01:59 +0200

    Create alternate-case.py

---
+++ 7-kyu/alternate-case.py
@@ -0,0 +1,7 @@
+def alternate_case(s):
+    return s.swapcase()
+
+'''
+Write function alternateCase which switch every letter in string from upper to
+lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+'''
