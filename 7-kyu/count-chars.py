b05314c2298ed46e0e25ada15f0bae36be1d865e
artbohr <bohrcs@gmail.com>
2018-07-25 02:36:39 +0200

    Create count-chars.py

---
+++ 7-kyu/count-chars.py
@@ -0,0 +1,14 @@
+def count_char(p,c):
+    return p.lower().count(c.lower())
+
+'''
+The goal of this kata is to write a function that takes two inputs: a string and a character.
+The function will count the number of times that character appears in the string. The count is case insensitive.
+
+For example:
+
+count_char("fizzbuzz","z") => 4
+count_char("Fancy fifth fly aloof","f") => 5
+
+The character can be any alphanumeric character.
+'''
