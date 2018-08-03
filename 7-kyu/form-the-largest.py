74ed315448575a84758014b95f586137bbc2245e
artbohr <bohrcs@gmail.com>
2018-08-04 01:49:59 +0200

    Create form-the-largest.py

---
+++ 7-kyu/form-the-largest.py
@@ -0,0 +1,8 @@
+def max_number(n):
+    return int("".join(sorted(list(str(n)), reverse=True)))
+
+'''
+Task
+
+Given a number , Return The Maximum number could be formed from the digits of the number given.
+'''
