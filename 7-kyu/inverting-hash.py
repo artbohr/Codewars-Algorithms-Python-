42063e3775e6c46885b2977393137799819b07c3
artbohr <bohrcs@gmail.com>
2018-08-05 05:04:58 +0200

    Create inverting-hash.py

---
+++ 7-kyu/inverting-hash.py
@@ -0,0 +1,43 @@
+def invert_hash(dictionary):
+    output = {}
+
+    for x,y in dictionary.items():
+        output[y] = x
+
+    return output
+
+'''
+Summary
+
+Given a Hash made up of keys and values, invert the hash by swapping them.
+Examples
+
+Given:
+
+  { 'a' : 1,
+    'b' : 2,
+    'c' : 3 }
+
+Return:
+
+  { 1 : 'a',
+    2 : 'b',
+    3 : 'c' }
+
+Given:
+
+  { 'foo'   : 'bar',
+    'hello' : 'world' }
+
+Return:
+
+  { 'bar'   : 'foo',
+    'world' : 'hello' }
+
+Notes
+
+    Keys and values may be of any type appropriate for use as a key.
+    All hashes provided as input will have unique values, so the inversion
+    is involutive. In other words, do not worry about identical values stored
+    under different keys.
+'''
