7765aab02cf3fd893d991310692b55851c139136
artbohr <bohrcs@gmail.com>
2018-08-26 03:58:48 +0200

    Create back-to-basics.py

---
+++ 7-kyu/back-to-basics.py
@@ -0,0 +1,15 @@
+def types(x):
+    return type(x).__name__
+
+'''
+Create a program that will return whether an input value is a str, int, float,
+or bool. Return the name of the value.
+
+Examples
+
+    Input = 23 --> Output = int
+    Input = 2.3 --> Output = float
+    Input = "Hello" --> Output = str
+    Input = True --> Output = bool
+
+'''
