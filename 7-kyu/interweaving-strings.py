ae555462ad3460acd8c42622bd3fa47b3215b550
artbohr <bohrcs@gmail.com>
2018-09-07 06:54:42 +0200

    Create interweaving-strings.py

---
+++ 7-kyu/interweaving-strings.py
@@ -0,0 +1,28 @@
+def interweave(s1, s2):
+    output = ''
+
+    for x in range(len(s1)):
+        if not s1[x].isdigit():
+            output += s1[x]
+        if x < len(s2) and not s2[x].isdigit():
+            output += s2[x]
+
+    return output
+
+'''
+Your friend Rick is trying to send you a message, but he is concerned that it
+would get intercepted by his partner. He came up with a solution:
+
+1) Add digits in random places within the message.
+
+2) Split the resulting message in two. He wrote down every second character on
+one page, and the remaining ones on another. He then dispatched the two messages separately.
+
+Write a function interweave(s1, s2) that reverses this operation to decode his message!
+
+Example 1: interweave("hlo", "el") -> "hello" Example 2: interweave("h3lo", "el4") -> "hello"
+
+Rick's a bit peculiar about his formats. He would feel ashamed if he found out his message
+led to extra white spaces hanging around the edges of his message...
+
+'''
