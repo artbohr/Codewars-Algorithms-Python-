79461d3f9021e7eb31f6f2ba5a526f92425bcc1d
artbohr <bohrcs@gmail.com>
2018-09-12 02:25:24 +0200

    Create deadfish-swim.py

---
+++ 6-kyu/deadfish-swim.py
@@ -0,0 +1,31 @@
+def parse(data):
+    num = 0
+    output = []
+
+    for x in data:
+        if x=='s':
+            num = num ** 2
+        elif x=='i':
+            num += 1
+        elif x=='d':
+            num -= 1
+        elif x=='o':
+            output.append(num)
+
+    return output
+
+'''
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+    i increments the value (initially 0)
+    d decrements the value
+    s squares the value
+    o outputs the value into the return array
+
+Invalid characters should be ignored.
+
+parse("iiisdoso")  ==>  [8, 64]
+
+'''
