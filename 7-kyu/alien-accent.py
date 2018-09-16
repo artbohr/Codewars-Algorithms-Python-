120abd0c33bfd4edce6ba044ffc208f88ff30919
artbohr <bohrcs@gmail.com>
2018-09-16 06:51:50 +0200

    Create alien-accent.py

---
+++ 7-kyu/alien-accent.py
@@ -0,0 +1,21 @@
+def convert(st):
+    return st.replace('o','u').replace('a','o')
+
+'''
+The Story:
+Aliens from Kepler 27b have immigrated to Earth! They have learned English and
+go to our stores, eat our food, dress like us, ride Ubers, use Google, etc.
+However, they speak English a little differently. Can you write a program that
+converts their Alien English to our English?
+
+Task:
+
+Write a function converting their speech to ours.
+They tend to speak the letter a like o and o like a u.
+
+>>> convert('hello')
+'hellu'
+>>> convert('codewars')
+'cudewors'
+
+'''
