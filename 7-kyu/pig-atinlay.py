e14aa90efaed0861b63f096589f629abc8f419cb
artbohr <bohrcs@gmail.com>
2018-10-07 08:53:54 +0300

    Create pig-atinlay.py

---
+++ 7-kyu/pig-atinlay.py
@@ -0,0 +1,15 @@
+def pig_latin(word):
+    return word[1:] + word[0] + 'ay' if len(word) > 3 else word
+
+'''
+Task:
+
+Make a function that converts a word to pig latin. The rules of pig latin are:
+
+If the word has more than 3 letters:
+  1. Take the first letter of a word and move it to the end
+  2. Add -ay to the word
+Otherwise leave the word alone.
+
+Example: hello = ellohay
+'''
