7ea828efb1eb534aa1d66620a3a80d3b68d8ec77
artbohr <bohrcs@gmail.com>
2018-11-20 15:07:46 +0100

    Create bug-squish.py

---
+++ 7-kyu/bug-squish.py
@@ -0,0 +1,20 @@
+def debug(s):
+    return s.replace('bugs','=').replace('bug','').replace('=','bugs')
+
+'''
+Take debugging to a whole new level:
+
+Given a string, remove every single bug.
+
+This means you must remove all instances of the word 'bug' from within a
+given string, unless the word is plural ('bugs').
+
+For example, given 'obugobugobuoobugsoo', you should return 'ooobuoobugsoo'.
+
+Another example: given 'obbugugo', you should return 'obugo'.
+
+Note that all characters will be lowercase and letters.
+
+Happy squishing!
+
+'''
