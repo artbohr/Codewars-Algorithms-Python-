459ccee073509ea11275fe3d0176513f92219b87
artbohr <bohrcs@gmail.com>
2018-12-15 16:38:18 +0100

    Create keypad-horror.py

---
+++ 7-kyu/keypad-horror.py
@@ -0,0 +1,21 @@
+def computer_to_phone(numbers):
+    return numbers.translate(str.maketrans('7894561230', '1234567890'))
+
+'''
+Having two standards for a keypad layout is inconvenient!
+Computer keypad's layout:
+7 8 9 \n 4 5 6 \n 1 2 3 \n 0 \n
+
+Cell phone keypad's layout:
+1 2 3\n 4 5 6\n 7 8 9\n 0\n
+
+Solve the horror of unstandartized keypads by providing a function that converts
+computer input to a number as if it was typed by a phone.
+
+Example:
+"789" -> "123"
+
+Notes:
+You get a string with numbers only
+
+'''
