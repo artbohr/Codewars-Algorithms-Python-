d8b864a5333794dc353c35fc7343bbc9f3e6832e
artbohr <bohrcs@gmail.com>
2019-01-05 06:06:07 +0100

    Create grains.py

---
+++ 7-kyu/grains.py
@@ -0,0 +1,15 @@
+def square(number):
+    return 2**(number-1)
+
+'''
+Grains
+
+Write a program that calculates the number of grains of wheat on a chessboard
+ given that the number on each square is double the previous one.
+
+There are 64 squares on a chessboard.
+
+#Example: square(1) = 1 square(2) = 2 square(3) = 4 square(4) = 8 etc...
+
+Write a program that shows how many grains were on each square
+'''
