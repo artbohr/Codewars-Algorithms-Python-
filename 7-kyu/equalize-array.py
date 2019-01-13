654c40f56d684f7637669745ed8f85a17110d941
artbohr <bohrcs@gmail.com>
2019-01-13 05:59:06 +0100

    Create equalize-array.py

---
+++ 7-kyu/equalize-array.py
@@ -0,0 +1,13 @@
+def equalize(arr):
+    return ['+'+str(x-arr[0]) if x-arr[0]>0-1 else str(x-arr[0]) for x in arr]
+
+'''
+No description!!!
+
+Input :: [10,20,25,0]
+
+Output :: ["+0", "+10", "+15", "-10"]
+
+Show some love, rank and upvote!
+
+'''
