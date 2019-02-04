5aab29f20ee98a804e59c9b77f0ae77365b56ab2
artbohr <bohrcs@gmail.com>
2019-02-04 10:54:19 +0100

    Create beads-count.py

---
+++ 7-kyu/beads-count.py
@@ -0,0 +1,9 @@
+def count_red_beads(n):
+    return 0 if n < 2 else (n-1)*2
+
+'''
+Two red beads are placed between every two blue beads.
+ There are N blue beads.
+
+If there are less than 2 blue beads return 0.
+'''
