9a15d3b3ee14ae5ba6f0437b0abc3ec16cfe3c5b
artbohr <bohrcs@gmail.com>
2019-02-25 11:48:49 +0100

    Create vowel-one.py

---
+++ 7-kyu/vowel-one.py
@@ -0,0 +1,26 @@
+def vowel_one(s):
+    output = ""
+
+    for letter in s:
+        if letter in "aeiouAEIOU":
+            output+='1'
+        else:
+            output+='0'
+
+    return output
+
+'''
+vowelOne
+
+Write a function that takes a string and outputs a strings of 1's and 0's where
+ vowels become 1's and non-vowels become 0's.
+
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+Examples:
+
+vowelOne "abceios" -- "1001110"
+
+vowelOne "aeiou, abc" -- "1111100100"
+
+'''
