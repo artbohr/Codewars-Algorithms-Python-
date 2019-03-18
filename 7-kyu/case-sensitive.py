e596d17e8101426cb4b5ddf50f4c5d0b1c439e32
artbohr <bohrcs@gmail.com>
2019-03-18 01:44:37 +0100

    Create case-sensitive.py

---
+++ 7-kyu/case-sensitive.py
@@ -0,0 +1,14 @@
+def case_sensitive(s):
+    low = [x for x in s if x==x.upper()]
+    return [low == [], low]
+
+
+'''
+Your task is very simple. Given an input string s, case_sensitive(s),
+ check whether all letters are lowercase or not. Return True/False and
+ a list of all the entries that are not lowercase in order of their
+ appearance in s.
+
+For example, case_sensitive('codewars') returns [True, []],
+ but case_sensitive('codeWaRs') returns [False, ['W', 'R']].
+'''
