6d85130c38398b460512b2e5f72ca99d8ae3b034
artbohr <bohrcs@gmail.com>
2019-04-04 13:20:38 +0200

    Create substring-instance-count.py

---
+++ 7-kyu/substring-instance-count.py
@@ -0,0 +1,12 @@
+def solution(full_text, search_text):
+    return full_text.count(search_text)
+
+'''
+Complete the solution so that it returns the number of times the search_text
+ is found within the full_text.
+
+Usage example:
+
+solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+solution('aaabbbcccc', 'bbb') # should return 1
+'''
