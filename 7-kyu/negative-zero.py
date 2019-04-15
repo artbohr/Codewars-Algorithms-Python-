ab2e94549952b9dce54d097d99f06f1f99497676
artbohr <bohrcs@gmail.com>
2019-04-15 02:24:50 +0200

    Create negative-zero.py

---
+++ 7-kyu/negative-zero.py
@@ -0,0 +1,9 @@
+def is_negative_zero(n):
+    return n == 0 and str(n)[0] == '-'
+    
+'''
+There exist two zeroes: +0 (or just 0) and -0.
+
+Write a function that returns true if the input number is -0 and
+ false otherwise (True and False for Python).
+'''
