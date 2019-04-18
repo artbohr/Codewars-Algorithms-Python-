dd5f6af28659730a649af24a4206363146d16fc3
artbohr <bohrcs@gmail.com>
2019-04-18 17:31:43 +0200

    Create local-talk.py

---
+++ 7-kyu/local-talk.py
@@ -0,0 +1,14 @@
+def pak(s):
+    return s.replace(" ", " pak ")
+
+'''
+In Bali, as far as I can gather, when ex-pats speak to locals, they basically
+ insert the word 'Pak' as often as possible. I am assured it means something
+ like 'mate' or 'sir' but that could be completely wrong.
+
+This kata requires you to turn any sentence provided (s) into ex-pat balinese
+ waffle by inserting the word 'pak' between every other word.
+
+Pak should not be the first or last word.
+ Strings of just spaces should return an empty string.
+'''
