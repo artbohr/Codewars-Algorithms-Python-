52c8e1a3777479ad17d562c5ef0d6465d4e7f98b
artbohr <bohrcs@gmail.com>
2019-04-27 14:53:50 +0200

    Create more-than-zero.py

---
+++ 7-kyu/more-than-zero.py
@@ -0,0 +1,10 @@
+def corrections(x):
+    if x > 0:
+        return '{} is more than zero.'.format(x)
+    return '{} is equal to or less than zero.'.format(x)
+
+'''
+Correct this code so that it takes one argument, x, and returns "x is more than zero"
+ if x is positive (and nonzero), and otherwise, returns "x is equal to or less than zero."
+ In both cases, replace x with the actual value of x.
+'''
