3b96f9f9d787ae3e0d0843bcaa37afba2fb3a96a
artbohr <bohrcs@gmail.com>
2019-05-01 03:46:05 +0200

    Create how-many-cl.py

---
+++ 7-kyu/how-many-cl.py
@@ -0,0 +1,24 @@
+def countWins(winnerList, country):
+    counter = 0
+    for x in winnerList:
+        if x['country'] == country:
+            counter +=1
+    return counter
+
+'''
+Create a function that takes two arguments:
+
+1) An array of objects which feature the season, the team and the country
+ of the Champions League winner.
+
+2) Country (as a string, for example, 'Portugal')
+
+You function should then return the number which represents the number of
+ times a team from a given country has won. Return 0 if there have been no wins.
+
+For example if the input array is as follows:
+
+countWins(winnerList1, 'Spain') => should return 2
+countWins(winnerList1, 'Portugal') => should return 1
+countWins(winnerList1, 'Sportland') => should return 0
+'''
