f113995aecbe946bf4c299998a953da2130fedda
artbohr <bohrcs@gmail.com>
2019-05-21 03:22:21 +0200

    Create filter-bug-fixing.py

---
+++ 7-kyu/filter-bug-fixing.py
@@ -0,0 +1,8 @@
+def filter_numbers(string):
+    return "".join(x for x in string if not x.isdigit())
+
+'''
+Failed Filter - Bug Fixing #3
+Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the
+ FilterNumber function to remove all the numbers from the string. 
+'''
