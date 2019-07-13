2720ff0510260797c02307641f647181c1c9ad6c
artbohr <bohrcs@gmail.com>
2019-07-14 02:02:21 +0300

    Create all-whitespace.py

---
+++ 7-kyu/all-whitespace.py
@@ -0,0 +1,8 @@
+def whitespace(string):
+    return string.isspace() or string == ''
+
+'''
+Implement whitespace(str) (Python), which should return true/True if given object
+ consists exclusively of zero or more whitespace characters, false/False otherwise.
+
+'''
