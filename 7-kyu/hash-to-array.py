e7fdf5c3e704e587aedfd8dd6b934071f9f2dee6
artbohr <bohrcs@gmail.com>
2019-08-18 21:59:33 +0200

    Create hash-to-array.py

---
+++ 7-kyu/hash-to-array.py
@@ -0,0 +1,14 @@
+def convert_hash_to_array(hash):
+    return sorted([[k,v] for k,v in hash.items()])
+
+'''
+Convert a hash into an array. Nothing more, Nothing less.
+
+{name: 'Jeremy', age: 24, role: 'Software Engineer'}
+
+should be converted into
+
+[["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
+
+Note: The output array should be sorted alphabetically.
+'''
