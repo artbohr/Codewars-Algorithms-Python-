1363faef37ff8be45b07ac14174420f8bd3a3261
artbohr <bohrcs@gmail.com>
2019-09-17 22:46:05 +0200

    Create ascii-letters-from-number.py

---
+++ 7-kyu/ascii-letters-from-number.py
@@ -0,0 +1,20 @@
+def convert(number):
+    return "".join(chr(int(number[x:x+2])) for x in range(0, len(number), 2))
+
+'''
+You have to create a function that converts integer given as string into ASCII
+ uppercase letters.
+
+All ASCII characters have their numerical order in table.
+
+For example,
+
+from ASCII table, character of number 65 is "A".
+
+Numbers will be next to each other, So you have to split given number to two
+ digit long integers.
+
+For example,
+
+'658776' to [65, 87, 76] and then turn it into 'AWL'.
+'''
