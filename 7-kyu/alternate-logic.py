98003658ca233e8e786baf50b9cd9e502250d6b3
artbohr <bohrcs@gmail.com>
2019-09-18 05:04:26 +0200

    Create alternate-logic.py

---
+++ 7-kyu/alternate-logic.py
@@ -0,0 +1,10 @@
+def alt_or(lst):
+    return True in lst if lst else None
+
+'''
+Create an OR function, without use of the 'or' keyword, that takes an list of boolean
+ values and runs OR against all of them.
+
+Assume there will be between 1 and 6 variables, and return None for an empty list.
+
+'''
