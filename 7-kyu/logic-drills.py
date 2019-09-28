f0fc589eb89df7e221d1db7d4421fd2e28ec2ed1
artbohr <bohrcs@gmail.com>
2019-09-29 01:12:38 +0200

    Create logic-drills.py

---
+++ 7-kyu/logic-drills.py
@@ -0,0 +1,27 @@
+def take_umbrella(weather, rain_chance):
+    if weather=='rainy':
+        return True
+    if weather=='sunny' and rain_chance>0.50:
+        return True
+    elif weather=='cloudy'and rain_chance>0.20:
+        return True
+    else:
+        return False
+
+'''
+Write a function take_umbrella() that takes two arguments: a string representing
+ the current weather and a float representing the chance of rain today.
+
+Your function should return True or False based on the following criteria.
+
+    You should take an umbrella if it's currently raining or if it's cloudy and the
+     chance of rain is over 0.20.
+    You shouldn't take an umbrella if it's sunny unless it's more likely to rain than not.
+
+The options for the current weather are sunny, cloudy, and rainy.
+
+For example, take_umbrella('sunny', 0.40) should return False.
+
+As an additional challenge, consider solving this kata using only logical operaters and not
+ using any if statements
+'''
