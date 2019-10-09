a877577b94cd7feab77efda077357f964cc8cad7
artbohr <bohrcs@gmail.com>
2019-10-09 21:32:37 +0200

    Create string-sum-fix.py

---
+++ 7-kyu/string-sum-fix.py
@@ -0,0 +1,10 @@
+def add(s1, s2):
+    return sum(ord(n) for n in s1+s2)
+
+'''
+Help Johnny!
+He can't make his code work!
+Easy Code
+Johnny is trying to make a function that adds the sum of two encoded strings,
+ but he can't find the error in his code! Help him!
+'''
