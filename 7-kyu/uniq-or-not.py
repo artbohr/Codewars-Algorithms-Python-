a358406fa098cbbef970779cc621356b7b4c4ff0
artbohr <bohrcs@gmail.com>
2019-10-27 16:31:59 +0100

    Create uniq-or-not.py

---
+++ 7-kyu/uniq-or-not.py
@@ -0,0 +1,14 @@
+def testit(a, b):
+    return sorted(list(set(a)) + list(set(b)))
+
+'''
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+
+https://www.codewars.com/kata/thinking-and-testing-uniq-or-not-uniq/python
+'''
